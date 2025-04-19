ba8f63c1ada9b03de9ae60cb3f59b429095a4771
Ubaidullah-Hasan <hm2964133@gmail.com>
2025-04-19 07:14:20 +0600

    array

--- finalExam/src/Encapsulation/PaymentGateWaySystem/Main.java
+++ finalExam/src/Encapsulation/PaymentGateWaySystem/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         User hasan = new User("Hasan", "hasan@gmail.com", 90.32);
 
-        PaymentMethod paypal = new Paypal(hasan);
-        PaymentMethod card = new CreditCard(hasan);
+        Paypal paypal = new Paypal(hasan);
+        CreditCard card = new CreditCard(hasan);
         PaymentMethod bkash = new Bkash(hasan);
 
         card.pay();
---
+++ finalExam/src/array/ArrayListPr.java
@@ -0,0 +1,17 @@
+package array;
+// import java.util.ArrayList;
+import java.util.ArrayList;
+
+public class ArrayListPr {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        ArrayList<String> cars = new ArrayList<String>();
+
+        int[] n = new int[5];
+        ArrayList<String> ab = new ArrayList<String>();
+
+        cars.add("A");
+        cars.get(0);
+    }
+    
+}
